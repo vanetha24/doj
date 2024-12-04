697997d1cb40e19e4d75fa8d7888c44a63bcf8e1
vanetha <vvanetha633@gmail.com>
2024-12-04 22:09:03 +0530

    add

---
+++ doj.js
@@ -0,0 +1,12 @@
+<script>
+window.embeddedChatbotConfig = {
+chatbotId: "XQ_p3k8dnaMvyUMnoWb4X",
+domain: "www.chatbase.co"
+}
+</script>
+<script
+src="https://www.chatbase.co/embed.min.js"
+chatbotId="XQ_p3k8dnaMvyUMnoWb4X"
+domain="www.chatbase.co"
+defer>
+</script>
